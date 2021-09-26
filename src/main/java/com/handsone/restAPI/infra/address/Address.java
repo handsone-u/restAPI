b4ba419579cb3d295056b6aefdefaece4915f756
handsone-u <jk04172@naver.com>
2021-09-26 18:52:35 +0900

    Remove field(Address::si)

--- src/main/java/com/handsone/restAPI/infra/address/Address.java
+++ src/main/java/com/handsone/restAPI/infra/address/Address.java
@@ -1,25 +1,13 @@
 package com.handsone.restAPI.infra.address;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Embeddable;
 
-@Embeddable @ToString @Getter @Setter
+@Embeddable @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String gu;
     private String dong;
-
-    public Address(String si, String gu, String dong) {
-        this.gu = gu;
-        this.dong = dong;
-    }
-
-    public Address(String gu, String dong) {
-        this.gu = gu;
-        this.dong = dong;
-    }
 }
