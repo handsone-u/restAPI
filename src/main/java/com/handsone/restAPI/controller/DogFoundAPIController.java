9d271457a0449aafd56c964d3f6aeecb8a72583f
handsone-u <jk04172@naver.com>
2021-11-14 22:33:13 +0900

    [추가]: 게시물 등록 시 인공지능 모델 호출 메서드 - DogLostAPIController.getDogFoundByDogBreed - DogFoundAPIController.getDogLostAndUpdateDogBreed

--- src/main/java/com/handsone/restAPI/controller/DogFoundAPIController.java
+++ src/main/java/com/handsone/restAPI/controller/DogFoundAPIController.java
@@ -1,6 +1,7 @@
 package com.handsone.restAPI.controller;
 
 import com.handsone.restAPI.domain.DogFound;
+import com.handsone.restAPI.dto.RequestAI;
 import com.handsone.restAPI.service.DogFoundService;
 import com.handsone.restAPI.dto.DogDto;
 import com.handsone.restAPI.global.response.CommonResponse;
@@ -13,12 +14,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
@@ -32,10 +36,13 @@ public class DogFoundAPIController {
     private final ModelMapper modelMapper;
     private final String downFoundPath = "/download-image-file/found/";
 
+    private final String aiModelHost = "localhost";
+    private final int aiModelPort = 8080;
+    private final String aiModelPath = "/ai-model";
+
     @PostMapping("")
     public ResponseEntity<? extends Response> postDogFound(
-            @ModelAttribute DogDto dogDto,
-            @RequestParam("files")List<MultipartFile> files) throws Exception{
+            @ModelAttribute DogDto dogDto, @RequestParam("files")List<MultipartFile> files) throws Exception{
         DogFound upload = dogFoundService.upload(dogDto);
         imageFileService.foundUpload(upload, files);
 
@@ -44,9 +51,16 @@ public class DogFoundAPIController {
                 .path(upload.getId().toString())
                 .toUriString();
 
+        DogDto dto = modelMapper.map(upload, DogDto.class)
+                .setMemberAndFileProperties(baseUri);
+
+        RequestAI requestAI = new RequestAI(dto.getImageFileList().get(0).getFilePath(),
+                dto.getId(), dto.getDogBreed(), "found");
+        getDogLostAndUpdateDogBreed(requestAI);
+
+        log.info("response={}", dto.getId());
         return ResponseEntity.ok()
-                .body(new CommonResponse<>(modelMapper.map(upload, DogDto.class)
-                        .setMemberAndFileProperties(baseUri)));
+                .body(new CommonResponse<>(dto));
     }
 
     @GetMapping("/{dogId}")
@@ -57,12 +71,12 @@ public class DogFoundAPIController {
     }
 
     @GetMapping("/dog-founds")
-    public ResponseEntity<Page> getAllDogFound(
-            @PageableDefault(size = 10, page = 0, sort = "id", direction = Sort.Direction.DESC)Pageable pageable) {
-        log.debug("currentRequestUri = [{}]", ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
+    public ResponseEntity<Page> getAllDogFound(@PageableDefault(sort = "id", direction = Sort.Direction.DESC)
+                                                           Pageable pageable) {
+        /*log.debug("currentRequestUri = [{}]", ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
         log.debug("pageable.PageNumber[{}]", pageable.getPageNumber());
         log.debug("pageable.PageSize[{}]", pageable.getPageSize());
-        log.debug("pageable.PageOffset[{}]", pageable.getOffset());
+        log.debug("pageable.PageOffset[{}]", pageable.getOffset());*/
         Page<DogFound> all = dogFoundService.findAll(pageable);
 
         String baseUri = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -72,5 +86,25 @@ public class DogFoundAPIController {
         return ResponseEntity.ok()
                 .body(all.map(d -> modelMapper.map(d, DogDto.class)
                         .setMemberAndFileProperties(baseUri + d.getId())));
+    }
+
+    private void getDogLostAndUpdateDogBreed(RequestAI requestAI) {
+        log.info("call AI Model, original Param:[{}]",requestAI);
+
+        UriComponents baseUri = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host(aiModelHost)
+                .port(aiModelPort)
+                .build();
+        log.info("requestBaseUri={}", baseUri.toUriString());
+
+        WebClient webClient = WebClient.create(baseUri.toUriString());
+        webClient.post()
+                .uri(aiModelPath)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(requestAI)
+                .retrieve()
+                .bodyToMono(RequestAI.class)
+                .subscribe(r -> log.info("result=[{}]", r));
     }
 }
--- src/main/java/com/handsone/restAPI/controller/DogLostAPIController.java
+++ src/main/java/com/handsone/restAPI/controller/DogLostAPIController.java
@@ -1,6 +1,7 @@
 package com.handsone.restAPI.controller;
 
 import com.handsone.restAPI.domain.DogLost;
+import com.handsone.restAPI.dto.RequestAI;
 import com.handsone.restAPI.service.DogLostService;
 import com.handsone.restAPI.dto.DogDto;
 import com.handsone.restAPI.global.response.CommonResponse;
@@ -13,17 +14,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/dog-lost")
+@Slf4j
 public class DogLostAPIController {
 
     private final DogLostService dogLostService;
@@ -31,9 +36,13 @@ public class DogLostAPIController {
     private final ModelMapper modelMapper;
     private final String downLostPath = "/download-image-file/lost/";
 
+    private final String aiModelHost = "localhost";
+    private final int aiModelPort = 8080;
+    private final String aiModelPath = "/ai-model";
+
     @PostMapping("")
     public ResponseEntity<? extends Response> postDogLost(
-            @ModelAttribute DogDto dogDto, BindingResult bindingResult,
+            @ModelAttribute DogDto dogDto,
             @RequestParam("files") List<MultipartFile> files) throws Exception {
         DogLost upload = dogLostService.upload(dogDto);
         imageFileService.lostUpload(upload, files);
@@ -43,9 +52,16 @@ public class DogLostAPIController {
                 .path(upload.getId().toString())
                 .toUriString();
 
+        DogDto dto = modelMapper.map(upload, DogDto.class)
+                .setMemberAndFileProperties(baseUri);
+
+        RequestAI requestAI = new RequestAI(dto.getImageFileList().get(0).getFilePath(),
+                dto.getId(), dto.getDogBreed(),"lost");
+        getDogFoundByDogBreed(requestAI);
+
+        log.info("response={}", dto.getId());
         return ResponseEntity.ok().body(
-                new CommonResponse<>(modelMapper.map(upload, DogDto.class)
-                        .setMemberAndFileProperties(baseUri)));
+                new CommonResponse<>(dto));
     }
 
     @GetMapping("/{dogId}")
@@ -55,14 +71,16 @@ public class DogLostAPIController {
                 .body(new CommonResponse<>(modelMapper.map(dogLost, DogDto.class)));
     }
 
-    //TODO[추가] 업로드 시, 인공지능 모델 Server 에 POST 전송
+    @GetMapping("/dog-breed")
+    public ResponseEntity<? extends Response> getDogLostByDogBreed(String dogBreed) {
+        List<DogLost> result = dogLostService.findAllByDogBreed(dogBreed);
+
+        return null;
+    }
+
     @GetMapping("/dog-losts")
     public ResponseEntity<Page> getAllDogLost(
-            @PageableDefault(size = 10, page = 0, sort = "id", direction = Sort.Direction.DESC)Pageable pageable) {
-        log.debug("currentRequestUri = [{}]", ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
-        log.debug("pageable.PageNumber[{}]", pageable.getPageNumber());
-        log.debug("pageable.PageSize[{}]", pageable.getPageSize());
-        log.debug("pageable.PageOffset[{}]", pageable.getOffset());
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC)Pageable pageable) {
         Page<DogLost> all = dogLostService.findAll(pageable);
 
         String baseUri = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -70,7 +88,26 @@ public class DogLostAPIController {
                 .toUriString();
 
         return ResponseEntity.ok()
-                .body(all.map(d -> modelMapper.map(d, DogDto.class)
-                        .setMemberAndFileProperties(baseUri + d.getId())));
+                .body(all.map(d -> modelMapper.map(d, DogDto.class).setMemberAndFileProperties(baseUri + d.getId())));
+    }
+
+    private void getDogFoundByDogBreed(RequestAI requestAI) {
+        log.info("call AI Model, original Param:[{}]",requestAI);
+
+        UriComponents baseUri = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host(aiModelHost)
+                .port(aiModelPort)
+                .build();
+        log.info("requestBaseUri={}", baseUri.toUriString());
+
+        WebClient webClient = WebClient.create(baseUri.toUriString());
+        webClient.post()
+                .uri(aiModelPath)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(requestAI)
+                .retrieve()
+                .bodyToMono(RequestAI.class)
+                .subscribe(r -> log.info("result=[{}]", r));
     }
 }
