c805f83952b924f45073a73baf282ebd52293182
handsone-u <jk04172@naver.com>
2021-11-23 22:59:03 +0900

    [수정] : 단위 테스트 애플리케이션 설정을 개발용으로 바꿈

--- src/test/java/com/handsone/restAPI/repository/MemberRepositoryTest.java
+++ src/test/java/com/handsone/restAPI/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
@@ -21,9 +22,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 // 각각의 TEST 는 자동으로 Roll Back 됨.
 @DataJpaTest
 @ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ActiveProfiles(profiles = "dev")
 class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
-    @Autowired TestEntityManager entityManager;
     static List<Member> members = new ArrayList<>();
 
     @BeforeAll
